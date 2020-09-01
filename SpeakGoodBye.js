cc9177203a17c849c9183b47d615069655fe8733
Amit Tallur <61627962+Amittallur@users.noreply.github.com>
2020-09-01 16:25:55 +0530

    Add files via upload

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,11 +1,9 @@
-
-
+  
 (function(window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
-
+    var byeSpeaker = new Object();
+    var speakWord = "Good Bye";
+    byeSpeaker.speak = function speak(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.byeSpeaker = byeSpeaker;
 })(window);
